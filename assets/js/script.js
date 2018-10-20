41e5e4166b0633f3c17ac3db2da4018cfefecc4c
dianaserban <dianaserban@rocketmail.com>
2018-10-20 23:49:03 +0300

    Commit message

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+$(document).ready (
+
+        function(){
+	
+	
+        $('#inputGroupSelect3').change(function(){
+            $('.business-select').hide();
+            $('#' + $(this).val()).show();
+        });
+		
+		
+		
+		
+		
+		 
+		
+		
+		
+		
+		});
+		
+
+
+
+    
+
+      
+    
+	
+	
+	
+
+	
+	
+                
+             
+           
+	
+	 
+
+  
